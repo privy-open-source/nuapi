2c7b957231f24e8eb440cf0fbb7e1f27b7484b4a
Ade Novid <adenvt@gmail.com>
2023-10-19 10:12:16 +0700

    fix: plugin priority

--- playground/app.vue
+++ playground/app.vue
@@ -1,19 +1,7 @@
 <template>
   <div>
-    Nuxt module playground!
-    <pre>{{ users }}</pre>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-
-<script setup>
-import { onMounted, ref } from 'vue'
-import { useApi } from '../src/core'
-
-const users = ref([])
-
-onMounted(async () => {
-  const { data } = await useApi().get('https://reqres.in/api/users')
-
-  users.value = data
-})
-</script>
--- src/runtime/api.client.ts
+++ src/runtime/api.client.ts
@@ -1,6 +1,5 @@
 import {
   useRuntimeConfig,
-  useRequestEvent,
   defineNuxtPlugin,
 } from '#imports'
 import {
@@ -8,23 +7,16 @@ import {
   createApi,
   setApi,
 } from '@privyid/nuapi/core'
-import getURL from 'requrl'
 import { joinURL } from 'ufo'
 
 export default defineNuxtPlugin(() => {
-  const event   = useRequestEvent()
   const config  = useRuntimeConfig()
-  const host    = getURL(event?.node?.req)
+  const host    = window.location.origin
   const baseURL = joinURL(host, config.app.baseURL)
 
   const options: ApiConfig = {
     baseURL,
     headers: {},
-  }
-
-  if (process.server && options.headers) {
-    // Don't accept brotli encoding because Node can't parse it
-    options.headers['accept-encoding'] = 'gzip, deflate'
   }
 
   const instance = createApi(options)
